0cbdf65874ea0531889a98b02c45dd41afbc8f13
535180001 <elvin.535180001@stu.untar.ac.id>
2020-12-08 15:21:04 +0700

    done

--- index.js
+++ index.js
@@ -8,37 +8,69 @@ app.listen(port, () => {
 })
 
 app.get("/", (req, res) => {
-    db.getUserAcc().then((ress, err) => {
+    db.getUserAcc(req.body.usern,req.body.pass).then((ress, err) => {
         res.send(ress)
     });
 })
 
 app.get("/account/forget", (req, res) => {
-    db.forgetUserAcc().then((ress, err) => {
+    db.forgetUserAcc(req.body.usern,req.body.altermail).then((ress, err) => {
         res.send(ress)
     });
 })
 
 app.get("/account/add", (req, res) => {
-    db.insertUserAcc().then((ress, err) => {
+    db.insertUserAcc(req.body.altermail,req.body.usern,req.body.pass).then((ress, err) => {
         res.send(ress)
     });
 })
 
-app.get("/account/update", (req, res) => {
-    db.updateUserAcc().then((ress, err) => {
+app.get("/account/update/password", (req, res) => {
+    db.updateUserAcc(req.body.usern,req.body.pass).then((ress, err) => {
+        res.send(ress)
+    });
+})
+
+app.get("/account/update/username", (req, res) => {
+    db.updateUserAcc(req.body.usern,req.body.newusern).then((ress, err) => {
         res.send(ress)
     });
 })
 
 app.get("/account/update/pic", (req, res) => {
-    db.updateUserAccPicture().then((ress, err) => {
+    db.updateUserAccPicture(req.body.oldpic,req.body.newpic).then((ress, err) => {
         res.send(ress)
     });
 })
 
 app.get("/account/update/backpic", (req, res) => {
-    db.updateUserAccBackground().then((ress, err) => {
+    db.updateUserAccBackground(req.body.oldback,req.body.newback).then((ress, err) => {
+        res.send(ress)
+    });
+})
+
+app.get("/email", (req, res) => {
+    db.getAllEmail().then((ress, err) => {
+        res.send(ress)
+    });
+})
+
+app.get("/email/find", (req, res) => {
+    db.getEmail(req.body.id).then((ress, err) => {
+        res.send(ress)
+    });
+})
+
+
+app.get("/email/add", (req, res) => {
+    db.insertEmail(req.body.username,req.body.receiver,req.body.title
+        ,req.body.content,req.body.attachment).then((ress, err) => {
+        res.send(ress)
+    });
+})
+
+app.get("/email/del", (req, res) => {
+    db.deleteEmail(req.body.id).then((ress, err) => {
         res.send(ress)
     });
 })
